8c95562fe7a523aa89d56842fcbab06d1fcbf941
Jelmer Cormont <jcormont+git@gmail.com>
2019-07-18 14:55:50 +0800

    Fix import issue in app.ts/js

--- src/templates/js-webpack/src/app.js.ts
+++ src/templates/js-webpack/src/app.js.ts
@@ -1,7 +1,7 @@
 export const name = "src/app.js"
 export const file = `
 import { BrowserApplication } from "@typescene/webapp";
-import { MainActivity } from "./activities/main/activity";
+import MainActivity from "./activities/main/activity";
 
 BrowserApplication.run(
     MainActivity,
--- src/templates/ts-webpack/src/app.ts.ts
+++ src/templates/ts-webpack/src/app.ts.ts
@@ -1,7 +1,7 @@
 export const name = "src/app.ts"
 export const file = `
 import { BrowserApplication } from "@typescene/webapp";
-import { MainActivity } from "./activities/main/activity";
+import MainActivity from "./activities/main/activity";
 
 BrowserApplication.run(
     MainActivity,
